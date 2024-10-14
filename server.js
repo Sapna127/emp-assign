7ad8f7704c167747df6905598f6aa2c4495d1b94
Sapna Kul <kulsapna2210002@gmail.com>
2024-10-14 19:05:04 +0530

    fix:enable credentials for usefetch hook

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(cors({
-    origin: 'https://emppp.vercel.app', 
+    origin: ['https://emppp.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+}));
+
   
 server.use(middlewares);
 server.use(router);
--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,31 +1,35 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-const useFetch = (url) =>{
-    const [data,setData] = useState(null);
-    const [isPending,setIsPending] = useState(null);
-    const [error,setError] = useState(null);
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true); 
+    const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch(url)
-                .then(res=>{
-                    if(!res.ok){
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url, {
+                method: 'GET', 
+                credentials: 'include', 
+            })
+                .then(res => {
+                    if (!res.ok) {
                         throw Error("Error fetching employees");
                     }
                     return res.json();
                 })
-                .then(data=>{
+                .then(data => {
                     setData(data);
-                    setIsPending(false);
+                    setIsPending(false); 
                     setError(null);
                 })
-                .catch(err=>{
+                .catch(err => {
                     setIsPending(false);
                     setError(err.message);
-                })
-        },1000)
-    },[url])
-    return {data,isPending,error};
+                });
+        }, 1000);
+    }, [url]);
+
+    return { data, isPending, error };
 }
 
 export default useFetch;

c05c3070d2f2f48d699c6b51dd9a65f41b9eb313
Sapna Kul <kulsapna2210002@gmail.com>
2024-10-15 13:43:50 +0530

    fix: Vercel 404 issue

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     }
-  }
-})
+  },
+  build: {
+    outDir: 'dist', 
+  },
+  server: {
+    open: true,      
+    port: 3000,      
+  },
+  resolve: {
+    alias: {
+    }
+  },
+});
